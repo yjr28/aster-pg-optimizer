c971e0f41d8ddcb17e442c91d3d132e8d8057ed5
Daniel Ryu <yjryu2424@gmail.com>
2026-09-04 02:07:31 -0400

    feat: export audited corpus merge API

--- aster/data/__init__.py
+++ aster/data/__init__.py
@@ -1,4 +1,13 @@
 from .integrity import DatasetIntegrityReport, audit_dataset
+from .merge import CombinedDataset, CorpusInput, combine_datasets
 from .records import PlanObservation, Provenance
 
-__all__ = ["DatasetIntegrityReport", "PlanObservation", "Provenance", "audit_dataset"]
+__all__ = [
+    "DatasetIntegrityReport",
+    "CombinedDataset",
+    "CorpusInput",
+    "PlanObservation",
+    "Provenance",
+    "audit_dataset",
+    "combine_datasets",
+]
