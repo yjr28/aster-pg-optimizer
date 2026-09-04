17f0ecf441ff99fa2c3ca70baaa5d2d598992e17
Daniel Ryu <yjryu2424@gmail.com>
2026-09-04 01:54:19 -0400

    feat: export robustness split regimes

--- aster/experiments/__init__.py
+++ aster/experiments/__init__.py
@@ -7,7 +7,7 @@ from .evaluate import (
     evaluate_runtime_ranking,
     fallback_pareto_sweep,
 )
-from .split import HoldoutSplit, template_holdout
+from .split import HoldoutSplit, parameter_holdout, template_holdout, workload_holdout
 
 __all__ = [
     "FallbackCurvePoint",
@@ -18,5 +18,7 @@ __all__ = [
     "evaluate_ranking",
     "evaluate_runtime_ranking",
     "fallback_pareto_sweep",
+    "parameter_holdout",
     "template_holdout",
+    "workload_holdout",
 ]
