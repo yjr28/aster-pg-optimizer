7898ace14acf82b1386d2e21c8c5f3893f0c29e3
Daniel Ryu <yjryu2424@gmail.com>
2026-09-04 00:05:35 -0400

    feat: export ranking-objective baselines

--- aster/models/__init__.py
+++ aster/models/__init__.py
@@ -1,10 +1,18 @@
-from .baselines import PlanScorer, PostgresCostRanker, RidgeRuntimeModel
+from .baselines import (
+    PairwiseLogisticRanker,
+    PlanScorer,
+    PostgresCostRanker,
+    QueryNormalizedRidgeModel,
+    RidgeRuntimeModel,
+)
 from .ensemble import RuntimeEnsemble, RuntimePrediction, TrainingExample
 
 __all__ = [
     "PlanScorer",
     "PostgresCostRanker",
     "RidgeRuntimeModel",
+    "QueryNormalizedRidgeModel",
+    "PairwiseLogisticRanker",
     "RuntimeEnsemble",
     "RuntimePrediction",
     "TrainingExample",
