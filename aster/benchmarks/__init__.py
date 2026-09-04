0ec60bc8799170ee4cb323e41f107be668f2caa2
Daniel Ryu <yjryu2424@gmail.com>
2026-09-04 00:08:49 -0400

    feat: export workload benchmark aggregation

--- aster/benchmarks/__init__.py
+++ aster/benchmarks/__init__.py
@@ -4,10 +4,20 @@ from .paired import (
     PairedExecutionSample,
     run_paired_benchmark,
 )
+from .workload import (
+    WorkloadBenchmarkSummary,
+    WorkloadQueryBenchmark,
+    WorkloadVariantSummary,
+    summarize_workload_benchmark,
+)
 
 __all__ = [
     "DistributionSummary",
     "PairedBenchmarkResult",
     "PairedExecutionSample",
     "run_paired_benchmark",
+    "WorkloadQueryBenchmark",
+    "WorkloadVariantSummary",
+    "WorkloadBenchmarkSummary",
+    "summarize_workload_benchmark",
 ]
